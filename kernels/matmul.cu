a53ae612bbc33717b11ff7b11c8ecd4b2b47ad6f
PranavDeepakSathya <sathya.pranav.deepak@gmail.com>
2025-12-05 04:57:39 +0000

    hahahga

--- kernels/matmul.cu
+++ kernels/matmul.cu
@@ -23,7 +23,24 @@ constexpr int bK = 32;
 __global__ void matmul(__nv_bfloat16* A, __nv_bfloat16 *B, __nv_bfloat16 *C, 
                         const __grid_constant__ CUtensorMap tensor_map_A, const __grid_constant__ CUtensorMap tensor_map_B)
 {
+  __shared__ alignas(128) __nv_bfloat16 S0[(bM*bK) + (bK*bN)]; 
+  __shared__ alignas(128) __nv_bfloat16 S1[(bM*bK) + (bK*bN)]; 
+
+  int t = threadIdx.x; 
+  int b = blockIdx.x; 
+  int b_dim = blockDim.x; 
 
 
-
+  __shared__ barrier S0_E, S0_F, S1_E, S1_F; 
+  if (t == 0)
+  {
+    init(&S0_E, blockDim.x);
+    init(&S1_E, blockDim.x);
+    init(&S0_F, blockDim.x);
+    init(&S1_F, blockDim.x);
+    // Make initialized barrier visible in async proxy.
+    cde::fence_proxy_async_shared_cta();
+  }
+  __syncthreads(); 
+  
 }
