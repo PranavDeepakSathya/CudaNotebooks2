724024f769441af300bc42c58ddd1a4ac897dab6
PranavDeepakSathya <sathya.pranav.deepak@gmail.com>
2025-12-05 04:16:22 +0000

    woooho

---
+++ kernels/matmul.cu
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<cuda_runtime.h> 
+#include<cuda.h> 
+#include<mma.h> 
+#include<cuda_bf16.h>
+
+constexpr int M = 4096;
+constexpr int N = 4096;
+constexpr int K = 4096;
+constexpr int WGMMA_M = 16; 
+constexpr int WGMMA_N = 16; 
+constexpr int WGMMA_K = 16; 
+constexpr int n_prod = 1; 
+constexpr int n_cons = 4; 
+constexpr int bM = 32; 
+constexpr int bN = 32; 
+constexpr int bK = 32;
+
+
+__global__ void matmul(__nv_bfloat16* A, __nv_bfloat16 *B, __nv_bfloat16 *C, 
+                        const __grid_constant__ CUtensorMap tensor_map_A, const __grid_constant__ CUtensorMap tensor_map_B)
+{
+  __shared__ __nv_bfloat16 A0[bM][bK]; 
+  __shared__ __nv_bfloat16 B0[bK][bN]; 
+  __shared__ __nv_bfloat16 A1[bM][bK]; 
+  __shared__ __nv_bfloat16 B1[bK][bN]; 
+
+
+
+}
