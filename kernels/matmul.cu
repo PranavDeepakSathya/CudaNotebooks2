d4164fa00baa4d3e17341e53bb734f65236cca63
PranavDeepakSathya <sathya.pranav.deepak@gmail.com>
2025-12-10 06:36:11 +0000

    ahhhhhh

--- kernels/matmul.cu
+++ kernels/matmul.cu
@@ -18,7 +18,11 @@ constexpr int n_cons = 4;
 constexpr int bM = 32; 
 constexpr int bN = 32; 
 constexpr int bK = 32;
-
+//each block will do a 32x64 matmul
+constexpr int cta_M = 32; 
+constexpr int cta_N = 64;
+constexpr int cta_dim_M = M/cta_M
+constexpr int cta_dim_N = N/cta_N
 
 __global__ void matmul(__nv_bfloat16* A, __nv_bfloat16 *B, __nv_bfloat16 *C, 
                         const __grid_constant__ CUtensorMap tensor_map_A, const __grid_constant__ CUtensorMap tensor_map_B)
@@ -43,4 +47,22 @@ __global__ void matmul(__nv_bfloat16* A, __nv_bfloat16 *B, __nv_bfloat16 *C,
   }
   __syncthreads(); 
   
+}
+
+int main()
+{
+  int numBlocks;
+    int blockSize = 128; // Your specific block size
+    size_t dynamicSMemSize = 0; // Dynamic shared memory per block
+
+    // Calculates max blocks per SM based on registers/smem usage
+    cudaOccupancyMaxActiveBlocksPerMultiprocessor(
+        &numBlocks, 
+        myKernel, 
+        blockSize, 
+        dynamicSMemSize
+    );
+
+    printf("Max Active Blocks per SM for 'myKernel': %d\n", numBlocks);
+    return 0;
 }
